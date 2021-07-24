175619dd7b21470398b7eaf9cd1eb6f2f776d1a3
Klimov Code <74239490+KlimovCode@users.noreply.github.com>
2021-07-24 22:19:48 +0500

    Update 7_reverse_intenger.js

--- 7_reverse_intenger.js
+++ 7_reverse_intenger.js
@@ -2,6 +2,7 @@ function rev(x) {
   a = x.toString()
   a = a.split('').reverse().join('')
   a = parseInt(a)
+  if(a >= Math.pow(2, 31)) return 0
   return x < 0 ? a - (a*2) : a
 }
 console.log(rev(-86212))
